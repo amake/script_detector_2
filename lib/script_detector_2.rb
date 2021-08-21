e5b91b930c4b05db822458561edfdee82e2fb7ca
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-08-22 00:07:39 +0900

    Implement main API

--- lib/script_detector_2.rb
+++ lib/script_detector_2.rb
@@ -1,8 +1,75 @@
 # frozen_string_literal: true
 
 require_relative 'script_detector_2/version'
+require_relative 'script_detector_2/patterns.gen'
 
+# Detect the CJK script of a string
 module ScriptDetector2
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    # @param string [String]
+    # @return [Boolean]
+    def japanese?(string)
+      return true if string =~ /[\p{Hiragana}\p{Katakana}]/
+      return false if string =~ /\p{Hangul}/
+
+      kanji = string.scan(/\p{Han}/)
+      return false unless kanji.any?
+
+      kanji.all?(JAPANESE_PATTERN)
+    end
+
+    # @param string [String]
+    # @return [Boolean]
+    def chinese?(string)
+      return false if string =~ /[\p{Hiragana}\p{Katakana}\p{Hangul}]/
+
+      string =~ /\p{Han}/
+    end
+
+    # @param string [String]
+    # @return [Boolean]
+    def simplified_chinese?(string)
+      return false if string =~ /[\p{Hiragana}\p{Katakana}\p{Hangul}]/
+
+      hanzi = string.scan(/\p{Han}/)
+      return false unless hanzi.any?
+
+      hanzi.all?(SIMPLIFIED_CHINESE_PATTERN)
+    end
+
+    # @param string [String]
+    # @return [Boolean]
+    def traditional_chinese?(string)
+      return false if string =~ /[\p{Hiragana}\p{Katakana}\p{Hangul}]/
+
+      hanzi = string.scan(/\p{Han}/)
+      return false unless hanzi.any?
+
+      hanzi.all?(TRADITIONAL_CHINESE_PATTERN)
+    end
+
+    # @param string [String]
+    # @return [Boolean]
+    def korean?(string)
+      return true if string =~ /\p{Hangul}/
+      return false if string =~ /[\p{Hiragana}\p{Katakana}]/
+
+      hanja = string.scan(/\p{Han}/)
+      return false unless hanja.any?
+
+      hanja.all?(KOREAN_PATTERN)
+    end
+
+    # @param string [String]
+    # @return [Symbol]
+    def identify_script(string)
+      if japanese?(string) then :Jpan
+      elsif korean?(string) then :Kore
+      elsif traditional_chinese?(string) then :Hant
+      elsif simplified_chinese?(string) then :Hans
+      elsif chinese?(string) then :Hani
+      else :Zyyy
+      end
+    end
+  end
 end
--- test/script_detector_2_test.rb
+++ test/script_detector_2_test.rb
@@ -7,7 +7,38 @@ class ScriptDetector2Test < Minitest::Test
     refute_nil ::ScriptDetector2::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_japanese?
+    assert(ScriptDetector2.japanese?(' 私のホバークラフトは鰻でいっぱいです.'))
+    refute(ScriptDetector2.japanese?(' 我的气垫船充满了鳝鱼.'))
+    refute(ScriptDetector2.japanese?(' 我的氣墊船充滿了鱔魚.'))
+    refute(ScriptDetector2.japanese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+  end
+
+  def test_chinese?
+    refute(ScriptDetector2.chinese?(' 私のホバークラフトは鰻でいっぱいです.'))
+    assert(ScriptDetector2.chinese?(' 我的气垫船充满了鳝鱼.'))
+    assert(ScriptDetector2.chinese?(' 我的氣墊船充滿了鱔魚.'))
+    refute(ScriptDetector2.chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+  end
+
+  def test_simplified_chinese?
+    refute(ScriptDetector2.simplified_chinese?(' 私のホバークラフトは鰻でいっぱいです.'))
+    assert(ScriptDetector2.simplified_chinese?(' 我的气垫船充满了鳝鱼.'))
+    refute(ScriptDetector2.simplified_chinese?(' 我的氣墊船充滿了鱔魚.'))
+    refute(ScriptDetector2.simplified_chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+  end
+
+  def test_traditional_chinese?
+    refute(ScriptDetector2.traditional_chinese?(' 私のホバークラフトは鰻でいっぱいです.'))
+    refute(ScriptDetector2.traditional_chinese?(' 我的气垫船充满了鳝鱼.'))
+    assert(ScriptDetector2.traditional_chinese?(' 我的氣墊船充滿了鱔魚.'))
+    refute(ScriptDetector2.traditional_chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+  end
+
+  def test_korean?
+    refute(ScriptDetector2.korean?(' 私のホバークラフトは鰻でいっぱいです.'))
+    refute(ScriptDetector2.korean?(' 我的气垫船充满了鳝鱼.'))
+    refute(ScriptDetector2.korean?(' 我的氣墊船充滿了鱔魚.'))
+    assert(ScriptDetector2.korean?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
   end
 end
