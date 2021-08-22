4e978efdc5d1e50ea1920cb129501984da63e08e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-08-22 22:24:09 +0900

    Add tests with Latin string

--- test/script_detector_2_test.rb
+++ test/script_detector_2_test.rb
@@ -12,6 +12,7 @@ class ScriptDetector2Test < Minitest::Test
     refute(ScriptDetector2.japanese?(' 我的气垫船充满了鳝鱼.'))
     refute(ScriptDetector2.japanese?(' 我的氣墊船充滿了鱔魚.'))
     refute(ScriptDetector2.japanese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+    refute(ScriptDetector2.japanese?(' Hello, world!'))
   end
 
   def test_chinese?
@@ -19,6 +20,7 @@ class ScriptDetector2Test < Minitest::Test
     assert(ScriptDetector2.chinese?(' 我的气垫船充满了鳝鱼.'))
     assert(ScriptDetector2.chinese?(' 我的氣墊船充滿了鱔魚.'))
     refute(ScriptDetector2.chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+    refute(ScriptDetector2.chinese?(' Hello, world!'))
   end
 
   def test_simplified_chinese?
@@ -26,6 +28,7 @@ class ScriptDetector2Test < Minitest::Test
     assert(ScriptDetector2.simplified_chinese?(' 我的气垫船充满了鳝鱼.'))
     refute(ScriptDetector2.simplified_chinese?(' 我的氣墊船充滿了鱔魚.'))
     refute(ScriptDetector2.simplified_chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+    refute(ScriptDetector2.simplified_chinese?(' Hello, world!'))
   end
 
   def test_traditional_chinese?
@@ -33,6 +36,7 @@ class ScriptDetector2Test < Minitest::Test
     refute(ScriptDetector2.traditional_chinese?(' 我的气垫船充满了鳝鱼.'))
     assert(ScriptDetector2.traditional_chinese?(' 我的氣墊船充滿了鱔魚.'))
     refute(ScriptDetector2.traditional_chinese?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+    refute(ScriptDetector2.traditional_chinese?(' Hello, world!'))
   end
 
   def test_korean?
@@ -40,5 +44,6 @@ class ScriptDetector2Test < Minitest::Test
     refute(ScriptDetector2.korean?(' 我的气垫船充满了鳝鱼.'))
     refute(ScriptDetector2.korean?(' 我的氣墊船充滿了鱔魚.'))
     assert(ScriptDetector2.korean?(' 내 호버크라프트는 장어로 가득 차 있어요.'))
+    refute(ScriptDetector2.korean?(' Hello, world!'))
   end
 end
